efad2a24274a1c6de28b099c26fffd39957799b1
Bindu Prakash <bindu2305@gmail.com>
2019-03-21 02:50:52 +0000

    Completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -3,8 +3,11 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
+require_relative '../lib/store'
 
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Total Annual Revenue: " + Store.sum(:annual_revenue).to_s
+puts "Average Annual Revenue: " + Store.average(:annual_revenue).to_s
+puts "No of stores generating > 1M: " + Store.where("annual_revenue > ?", 1000000).count.to_s
