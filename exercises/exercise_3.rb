062149da4511e3256c5cb37dd83fd94392b8c658
Bindu Prakash <bindu2305@gmail.com>
2019-03-21 02:50:35 +0000

    Completed exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
+require_relative '../lib/store'
 
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.last
+puts Store.count
+@store3.destroy
+puts Store.count
