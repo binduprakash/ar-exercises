94bdc6f962c93bc92124cf4c444eaf2c97ee0e2b
Bindu Prakash <bindu2305@gmail.com>
2019-03-21 02:03:10 +0000

    Completed exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require_relative '../lib/store'
 
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.all[1]
+@store1.name = 'Burnaby North'
+@store1.save
+puts @store1.name
